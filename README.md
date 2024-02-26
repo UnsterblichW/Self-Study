7a303038bf5bf8e4d2a418ad0a63dfa0bd2d5823
Unsterblich <1163153301@qq.com>
2024-02-26 15:20:58 +0800

    fmt 注释

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 ## fmt 库怎么用？
 怎么在windows环境下加入到现有的vs项目中？
 https://blog.csdn.net/qq_45254369/article/details/129783588
+
--- Self-Study/main.cpp
+++ Self-Study/main.cpp
@@ -40,12 +40,27 @@ void TestThreadPoolSort() {
 //************************************************* ThreadPool *********************************//
 
 
-
+//************************************************* fmt ****************************************//
 #define FMT_HEADER_ONLY // (#define FMT_HEADER_ONLY)是强制性的，它告诉编译器也要编译fmt头文件
 #include <fmt/core.h>
+// 如果不写 FMT_HEADER_ONLY ，那么就得在项目设置里面把编译出来的dll或者lib链到当前需要调试的项目里面
+// 或者像下面这样写
+//
+//#ifndef _WIN64
+//    #ifdef _DEBUG
+//        #pragma comment(lib, "../lib32/debug/fmtd.lib")  路径得改
+//    #else
+//        #pragma comment(lib, "../lib32/release/fmt.lib")
+//    #endif
+//#else
+//    #ifdef _DEBUG
+//        #pragma comment(lib, "../lib64/debug/fmtd.lib")
+//    #else
+//        #pragma comment(lib, "../lib64/release/fmt.lib")
+//    #endif
+//#endif
 
-
-
+//************************************************* fmt ****************************************//
 
 int main()
 {
